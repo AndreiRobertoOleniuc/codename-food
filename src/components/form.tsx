dbe26c617eca957a9c96075b6cf06c33a195bbbf
Andrei Roberto Oleniuc <andrei.oleniuc@apiax.com>
2024-08-04 21:00:59 +0200

    chore: Refactor RecipeCreationForm component to include ingredient input and list

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,23 +1,47 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+
+import React, { FormEvent, useState } from "react";
 
 export function RecipeCreationForm() {
+  const [ingredients, setIngredients] = useState<string[]>([]);
+
+  const [ingredientInput, setIngredientInput] = useState("");
+
+  const addIngredient = (ingredient: string, e: FormEvent) => {
+    e.preventDefault();
+    setIngredientInput("");
+    setIngredients([...ingredients, ingredient]);
+  };
+
   return (
-    <form>
-      <div className="grid grid-cols-1 gap-6 mt-5">
-        <div className="flex w-full max-w-sm items-center space-x-2">
-          <Input type="text" placeholder="Ingredient" className="flex-grow" />
-          <Button type="submit">Add</Button>
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-primary text-background rounded-lg py-2"
-        >
-          Generate Recipe
-        </button>
+    <form className="grid grid-cols-1 gap-6 mt-5 max-w-96">
+      <div className="flex w-full items-center space-x-2">
+        <Input
+          type="text"
+          placeholder="Ingredient"
+          className="flex-grow"
+          value={ingredientInput}
+          onChange={(e) => setIngredientInput(e.target.value)}
+        />
+        <Button onClick={(e) => addIngredient(ingredientInput, e)}>Add</Button>
       </div>
+      <ul className="list-disc list-inside space-x-4 flex flex-row flex-wrap ">
+        {ingredients.map((ingredient, index) => (
+          <Badge key={index} className="py-2 my-2">
+            {ingredient}
+          </Badge>
+        ))}
+      </ul>
+      <button
+        type="submit"
+        className="w-full bg-primary text-background rounded-lg py-2"
+      >
+        Generate Recipe
+      </button>
     </form>
   );
 }
